bd4036d173126cec3197352a350d9dc26e4bbadd
Caio <85085155+CaioYusuke@users.noreply.github.com>
2022-03-20 20:45:31 -0300

    Update help.js

--- SlashCommands/utilidade/help.js
+++ SlashCommands/utilidade/help.js
@@ -9,15 +9,14 @@ module.exports = {
     run: async(client, interaction, args) => {
         
         const embed = new MessageEmbed()
-        .setTitle('ㅤㅤㅤㅤㅤ**Comandos**')
+        .setTitle('Comandos')
         .setThumbnail('https://i.imgur.com/iE2RDHy.png')
-        .setColor('#219100')
+        .setColor(interaction.member.displayColor)
         .setTimestamp()
         .addFields(
-            {name: 'Utilidade', value: 'ya ping     \n\ ya afk  \n\ ya avatar     \n\ ya serverinfo \n\ ya userinfo \n\ ya servericon \n\ ya banner', inline: true},
-            {name: 'Moderação', value: 'ya ban     \n\ ya mute     \n\ ya unban     ', inline: true},
-            {name: 'Diversão', value: 'ya slap \n\ ya kiss \n\ ya hug \n\ ya snake \n\ ya cowsay', inline: true},
-            {name: 'Bot', value: 'ya botinfo \n\ ya eval \n\ ya reload \n\ ya reportbug \n\ ya invite', inline: true},
+            {name: 'Utilidade', value: '/ping  \n\ /avatar \n\ /serverinfo \n\ /userinfo \n\ /servericon \n\ /banner', inline: true},
+            {name: 'Diversão', value: '/kiss \n\ /slap \n\ /hug \n\ /ya cowsay', inline: true},
+            {name: 'Bot', value: '/eval \n\ /invite', inline: true},
         )
 
         interaction.reply({content: `${interaction.user}`, embeds: [embed]})
